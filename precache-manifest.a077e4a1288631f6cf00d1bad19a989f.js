904fd618efaf900d6ca21c0c645fb0e0633d9ba2
trdmitr <trdmitr@gmail.com>
2023-03-03 14:29:02 +0400

    Updates

---
+++ precache-manifest.a077e4a1288631f6cf00d1bad19a989f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2667968478c1b77b901d",
+    "url": "/February-2023/static/css/main.38749bbb.chunk.css"
+  },
+  {
+    "revision": "2667968478c1b77b901d",
+    "url": "/February-2023/static/js/main.26679684.chunk.js"
+  },
+  {
+    "revision": "eb4a1c3d0c3ad6805649",
+    "url": "/February-2023/static/css/1.bbaf548f.chunk.css"
+  },
+  {
+    "revision": "eb4a1c3d0c3ad6805649",
+    "url": "/February-2023/static/js/1.eb4a1c3d.chunk.js"
+  },
+  {
+    "revision": "b823542356334228ae29",
+    "url": "/February-2023/static/js/runtime~main.b8235423.js"
+  },
+  {
+    "revision": "ee297f54893d78da45132f8d98d46824",
+    "url": "/February-2023/index.html"
+  }
+];
